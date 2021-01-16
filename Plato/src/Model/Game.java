0d318c45f88c800188e4d7876f9a986c35de1368
SadeghMahmoud <sadegh2040mah@gmail.com>
2021-01-16 11:52:57 +0330

    Plato Game

--- Plato/src/Model/Game.java
+++ Plato/src/Model/Game.java
@@ -1,4 +1,19 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Game {
+    private String gameName;
+
+    public Game(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void runGame(int player1ID, int player2ID) {
+
+    }
 }
