605603b252801db87b5451d6f0a1b9439cdb1811
SadeghMahmoud <sadegh2040mah@gmail.com>
2021-01-16 11:55:03 +0330

    Plato RunGame

--- Plato/src/Model/RunGame.java
+++ Plato/src/Model/RunGame.java
@@ -1,4 +1,11 @@
 package Model;
 
+import ModelReversi.PlayerReversi;
+
 public class RunGame {
+    private PlayerReversi playerReversi1;
+    private PlayerReversi playerReversi2;
+    private String gameName;
+
+
 }
